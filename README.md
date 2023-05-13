6ebfc5b1b1f400f072e3a38e6ab03438ddac46c7
Taavi Laanemaa <taavi.laanemaa@gmail.com>
2023-05-13 11:37:55 +0200

    Got OpenAI integration working

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # ai-stuff
+
 A space for testing all sorts of AI stuff
+
+## Resources
+
+- https://js.langchain.com/docs
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,2 @@
 import "dotenv/config";
-import { MathemApi } from "./tools/mathem/MathemApi.js";
-
-const api = new MathemApi()
-const products = await api.searchProducts("banana")
-console.log(products.map(x => x.fullName + " -> " + x.price))
+import "./chains/shoppingList.js";
